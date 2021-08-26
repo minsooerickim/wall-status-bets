50302e825761b0e332c5131999207cb0493fc347
Minsoo Kim <minsooerickim@gmail.com>
2021-08-25 20:35:36 -0700

    Pushshift update

--- client/my-app/pages/index.js
+++ client/my-app/pages/index.js
@@ -1,21 +1,27 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Axios from 'axios'
 import styles from '../styles/Home.module.css'
 
 import React, {useState, useEffect} from 'react'
 import Link from 'next/Link'
 import { csv } from 'd3'
+import axios from 'axios'
 
 export default function Home() {
+  //pushshift api
   const [data, setData] = useState([]);
-  var json = JSON.stringify(data);
-  json = JSON.parse(json);
-  
   useEffect(() => {
-    csv('nasdaqTickers.csv').then(setData)
-  }, []);
-
-  // console.log(json);
+    axios('https://api.pushshift.io/reddit/search/comment/?q=GME&subreddit=wallstreetbets&size=100&after=3h')
+    // axios('https://api.pushshift.io/reddit/search/submission/?q=TSLA&subreddit=wallstreetbets&size=0&after=1d&aggs=created_utc&frequency=day')
+      .then(response=> {
+        setData(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  },[])
+  console.log(data);
+  // console.log(data.data.length);
   return (  
     <div className={styles.container}>
       <Head>
@@ -26,31 +32,38 @@ export default function Home() {
 
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a>WallStatusBets.io!</a>
-        </h1>
+        <div>
+          <h1 className={styles.title}>
+            Welcome to <a>WallStatusBets.io!</a>
+          </h1>
 
-        <p className={styles.description}>
-          What do the people of the {' '}
-          <code className={styles.code}>r/wallstreetbets</code>
-          think?
-        </p>
+          <p className={styles.description}>
+            What do the people of the {' '}
+            <code className={styles.code}>r/wallstreetbets</code>
+            {' '} think?
+          </p>
+          <div className={styles.start}>
+            <Link href="/searchResult">Start!</Link>
+          </div>
+        </div>
 
-        <Link href="/searchResult">Rate</Link>
+        <div className={styles.trending}>
+          <h1>Trending <br></br></h1>
+          <p className={styles.description}><code className={styles.code}>r/wallstreetbets</code></p>
 
-
+          <p className={styles.description}><code className={styles.code}>WallStatusBets.io</code></p>
+          </div>
       </main>
       
+
+
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        <a classname={styles.github}
+          href="https://github.com/minsooerickim"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+        Powered by &nbsp;<span>Minsoo Kim</span>
         </a>
       </footer>
     </div>
